9a74d267a51473bd915696ff982f6c94fb78840a
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-02-20 11:16:35 -0500

    fixed type issues for subway page

--- app/[category]/subway-2016/page.tsx
+++ app/[category]/subway-2016/page.tsx
@@ -1,24 +1,94 @@
+"use client"; // client component
+import { useEffect, useState } from "react";
 import Link from 'next/link';
 import styles from "@/app/ui/subway.module.css";
 
+
 const returnStr = "<---";
 const links = [
-  "https://www.youtube.com/embed/spRQR7xBG3g?autoplay=1&rel=0&amp;showinfo=0",
-  "https://www.youtube.com/embed/-4kpwJKwZ8Q?autoplay=1&rel=0&amp;showinfo=0",
-  "https://www.youtube.com/embed/US-81APdmFM?autoplay=1&rel=0&amp;showinfo=0",
-  "https://www.youtube.com/embed/hawErPK95Ak?autoplay=1&rel=0&amp;showinfo=0",
-  "https://www.youtube.com/embed/bRrMaBHg8Ug?autoplay=1&rel=0&amp;showinfo=0",
-  "https://www.youtube.com/embed/1vIiw26kOrI?autoplay=1&rel=0&amp;showinfo=0",
-  "https://www.youtube.com/embed/PDeznrE5mVk?autoplay=1&rel=0&amp;showinfo=0",
-  "https://www.youtube.com/embed/4ZmNfeSgIpg?autoplay=1&rel=0&amp;showinfo=0",
-  "https://www.youtube.com/embed/xGTYk31Py3g?autoplay=1&rel=0&amp;showinfo=0",
+  "spRQR7xBG3g",
+  "-4kpwJKwZ8Q",
+  "US-81APdmFM",
+  "hawErPK95Ak",
+  "bRrMaBHg8Ug",
+  "1vIiw26kOrI",
+  "PDeznrE5mVk",
+  "4ZmNfeSgIpg",
+  "xGTYk31Py3g",
 ];
 
 function Subway() {
- 
+  const [players, setPlayers] = useState<YT.Player[]>([]);
+
+  
+  // Load YouTube API dynamically
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const script = document.createElement("script");
+      script.src = "https://www.youtube.com/iframe_api";
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.onload = () => {
+        if (window.YT && window.YT.Player) {
+          initializePlayers();
+        } else {
+          window.onYouTubeIframeAPIReady = initializePlayers;
+        }
+      };
+    }
+  }, []);
+  
+  const initializePlayers = () => {
+    const newPlayers = links.map((videoId, index) => {
+      return new window.YT.Player(`video-${index}`, {
+        videoId,
+        playerVars: {
+          autoplay: 1,
+          mute: 1,
+          rel: 0,
+          controls: 0,
+        },
+        events: {
+          onReady: (event: YT.PlayerEvent) => event.target.playVideo(),
+        },
+      });
+    });
+
+    setPlayers(newPlayers);
+  };
+  const muteAllVideos = () => {
+    players.forEach((player) => player?.mute());
+  };
+
+  const unmuteAllVideos = () => {
+    players.forEach((player) => player?.unMute());
+  };
+  const playAllVideos = () => {
+    players.forEach((player) => player?.playVideo());
+  };
+
+  const pauseAllVideos = () => {
+    players.forEach((player) => player?.pauseVideo());
+  };
 
   return (
+
     <div className={styles.mainContainer}>
+      <div id={styles.buttonWrapper}>
+        <button id={styles.button} onClick={muteAllVideos}>
+          Mute All
+        </button>
+        <button id={styles.button} onClick={unmuteAllVideos}>
+          Unmute All
+        </button>
+        <button id={styles.button} onClick={playAllVideos}>
+          Play All
+        </button>
+        <button id={styles.button} onClick={pauseAllVideos}>
+          Pause All
+        </button>
+      </div>
       <Link href="/webprojects">
         <div id={styles.returnLink}>
           {returnStr} <br />
@@ -29,13 +99,8 @@ function Subway() {
         </div>
       </Link>
       {
-        links.map((item, idx) => (
-          <iframe
-            className={styles.videos}
-            title={`part ${idx + 1} of a 9x9 grid of Jeremy Wiles-Young's subway web piece`}
-            src={item}
-            key={idx}
-          ></iframe>
+        links.map((_, idx) => (
+          <div key={idx} id={`video-${idx}`} className={styles.videos}></div>
         ))
       }
     </div>
