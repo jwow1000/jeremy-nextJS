cb226d1f5fbbfb662359c6025009de9f7fc8037a
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-07-15 22:53:49 -0400

    better url

--- app/[category]/immix/page.tsx
+++ app/[category]/immix/page.tsx
@@ -51,7 +51,7 @@ export default async function ImmixCollection() {
           <div className={styles.mixcloudWrapper}>
             {sounds.map((sound, idx) => (
               <AudioPlayer 
-                audioSrc={`audio/immix-soundfiles/${sound.link}`} 
+                audioSrc={`/audio/immix-soundfiles/${sound.link}`} 
                 key={`immix-track${idx}`}
                 title={sound.title}
               />
