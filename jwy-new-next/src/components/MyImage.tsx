ca4f269796f6dacf69fa5e18403668cf91e5e0cd
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-10-02 13:34:33 -0400

    fix sizes warning on Image

--- jwy-new-next/src/components/MyImage.tsx
+++ jwy-new-next/src/components/MyImage.tsx
@@ -18,6 +18,7 @@ export default function MyImage({ src, alt, objectFit='cover' }: FancyImageProps
       src={src}
       alt={alt}
       fill
+      sizes={`(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw`}
       style={{ objectFit: objectFit, objectPosition: 'center',  }}
       className={`fade-in ${loaded ? 'loaded' : ''}`}
       onLoad={() => setLoaded(true)}
