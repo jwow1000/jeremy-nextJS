e8c37ab5ec2743aa419f74ab5ae5df5c4b202ad7
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-11-10 17:31:05 -0500

    fix random hydration issue

--- jwy-new-next/src/app/ClientHome.tsx
+++ jwy-new-next/src/app/ClientHome.tsx
@@ -3,22 +3,27 @@
 import { Work } from "../../../jwy-website-studio/sanity.types"
 import Link from "next/link";
 import MyImage from "@/components/MyImage";
+import { useEffect, useState } from "react";
 
 interface ClientHomeParams {
   works: Work[];
 }
 export default function ClientHome({works}: ClientHomeParams) {
-  const randIdx = Math.floor(Math.random() * works.length);
-  const work = works[randIdx];
-      
+  const [work, setWork] = useState<Work>();
+
+  useEffect(() => {
+    const randIdx = Math.floor(Math.random() * works.length);
+    setWork(works[randIdx]);
+  }, [])
+
   return (
     work &&
-    <section className="w-2/3 h-full flex items-center">
-      <Link href={`works/${work.slug?.current}`} className="realtive w-full my-auto flex-col">
+    <section className="w-full md:w-2/3 h-full flex items-center">
+      <Link href={`works/${work.slug?.current}`} className="realtive w-full mx-auto flex-col">
         <h2 className="mx-auto w-98">{work.title}</h2>
         {
           work.featuredImage &&
-          <div className="relative w-98 h-98 mx-auto">
+          <div className="relative w-full md:w-98 h-98 mx-auto">
             <MyImage src={work.featuredImage} alt={work.featuredImage.alt || "no alt text"}/> 
           </div>
         }
